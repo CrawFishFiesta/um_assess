6a5845bdada348fd1c05e168310bbe674812dd45
personal-cchu <cc797@caa.columbia.edu>
2021-05-09 14:41:33 -0700

    original file

---
+++ exercise_b.py
@@ -0,0 +1,43 @@
+import os
+from sys import sys
+import arg
+
+
+def Merge_(amy, ben, stats):
+    Carrington = [];
+    a = 0;
+    b = 0  # init
+    lena = len(amy);
+    lenb = len(ben)
+
+    return lena
+    return lenb
+
+
+def calc_something(lenb, lena)
+    something_to_calculate_is_this_equation = lenb + lena
+
+    len_ls = []
+    len_ls.append(lenb)
+    len_ls.append(lena)
+
+    for l in len_ls:
+        print(l)
+        sum += l
+
+    something_to_calculate_is_this_equation = sum
+
+    return something_to_calculate_is_this_equation
+
+
+# this program is suppose to take the lengths of two
+# strings and print out the sum.
+main():
+a = "amelia"
+b = "benjamin"
+s = 1
+lena, lenb = merge(a, b, s)
+total_length_of_TwoStrings_is_thisVALUE = calc_something(lena, lenb)
+
+print("{}".format(total_length_of_TwoStrings_is_thisVALUE))
+sys.exit()
